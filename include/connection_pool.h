9eb27e3c269c09c25ce0c725e82cdec9cc1db1cc
harshit <harshittatwedi@gmail.com>
2025-01-31 03:13:02 +0530

    implemented return connection, added timeout

--- include/connection_pool.h
+++ include/connection_pool.h
@@ -6,10 +6,11 @@
 #include <condition_variable>
 #include <memory>
 #include <libpq-fe.h>
+#include <chrono>
 
 class ConnectionPool {
 public:
-    ConnectionPool(size_t poolSize, const std::string& connInfo);       
+    ConnectionPool(size_t poolSize, const std::string& connInfo, int timeout);       
     ~ConnectionPool();                                                  //cleans up and closes all connections
     std::unique_ptr<PGconn, Deleter> get_connection();                   //the thread asks for the connection using this function
     void return_connection(std::unique_ptr<PGconn, Deleter> conn);       //the thread returns the connection using this function
@@ -20,6 +21,7 @@ private:
     std::condition_variable poolCV;                                     //cv to notify threads waiting for a connection from the pool whenever available
     size_t poolSize;                                                    //hold pool size                            
     std::string connInfo;                                               //hold connection string
+    std::chrono::milliseconds timeoutDuration;                          //if it takes longer than this, we handout connection timed out
 
     //custom deleter functor for our unique pter, since PGConn doesnt use new and delete, prolly malloc and free
     struct Deleter {
@@ -32,6 +34,9 @@ private:
     // @param conn: A reference to a unique_ptr holding the PGconn object.
     // @return: True if the connection is valid, false otherwise.
     static bool check_pq_status(const std::unique_ptr<PGconn, Deleter>& conn);                                      //helper function to check PQstatus
+
+    //In case an old connection becomes invalid, we replace that connection by creating a new one
+    std::unique_ptr<PGconn, Deleter> make_new_connection();
 
     //initialise the connection pool, add the connection objs to the queue
     void initializePool();                              
